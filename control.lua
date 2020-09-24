49ad57d2913eb6b022f7c31a71921cbc67ed3855
dgflagg <github@dgflagg.info>
2020-09-23 20:54:48 -0400

    cleaned up moar stuff added lots of comments and removed uneeded source

--- control.lua
+++ control.lua
@@ -1,14 +1,9 @@
-script.on_event(defines.events.on_player_changed_position,
-  function(event)
-    local player = game.get_player(event.player_index) -- get the player that moved            
-    -- if they're wearing our armor
-    if player.character and player.get_inventory(defines.inventory.character_armor).get_item_count("fire-armor") >= 1 then
-       -- create the fire where they're standing
-       player.surface.create_entity{name="fire-flame", position=player.position, force="neutral"}
-       player.color = {r = 1, g = 0, b = 0, a = 0}
-    end
-  end
-)
+-- constants
+local NOTIFICATION_COLOR = {r = 0.7, g = 0, b = 0.3, a = 1}
+local REMOVE_COLOR = {r = 1, g = 0, b = 0, a = 1}
+local ADD_COLOR = {r = 0, g = 1, b = 0, a = 1}
+local LIST_COLOR = {r = 0, g = 0.7, b = 0.3, a = 1}
+
 
 function mysplit (inputstr, sep)
   if sep == nil then
@@ -31,107 +26,99 @@ function isListEmpty(todoList)
 end
 
 -- displays TODO list command options overview
-function displayHelpMessage(player, notificationColor, listColor, addColor, removeColor)
+function displayHelpMessage(player)
   player.clear_console()
-  player.print("The following commands are available:", notificationColor)
-  player.print("\t- list: shows the player's TODO list with index", listColor)
-  player.print("\t- add [item name]: add a new item to the TODO list", addColor)
-  player.print("\t- remove [item index]: removes item from TODO list at the index", removeColor)
+  player.print("The following commands are available:", NOTIFICATION_COLOR)
+  player.print("\t- list: shows the player's TODO list with index", LIST_COLOR)
+  player.print("\t- add [item name]: add a new item to the TODO list", ADD_COLOR)
+  player.print("\t- remove [item index]: removes item from TODO list at the index", REMOVE_COLOR)
 end
 
-function showTodoList(player, todoList, listColor)
+-- displays the current list of TODO items to the player
+function showTodoList(player, todoList)
   player.clear_console()
   if isListEmpty(todoList) then
-    player.print("TODO List empty!", listColor)
+    player.print("Nothing TODO!", LIST_COLOR)
   else
-    player.print("TODO: ", listColor)
+    player.print("TODO: ", LIST_COLOR)
     for k,v in pairs(todoList) do
-      player.print(k .. " - "..v, listColor)
+      player.print(k .. " - "..v, LIST_COLOR)
     end
   end
 end
 
-function addTodo(player, input, addColor)
+-- adds a new TODO item to the player's list
+function addTodo(player, inputs)
   player.clear_console()
   if global.todoList == nil then
     global.todoList = {}
   end
 
-  -- TODO: make this so it does not add the entire input with the "add" command keyword
+  -- strip the command word from the todo item
+  local todoItem = ""
+  for k,v in pairs(inputs) do
+    -- last item in list
+    if k == #inputs then
+      todoItem = todoItem .. v
+    elseif k ~= 1 then
+      todoItem = todoItem .. v .. " "
+    end
+  end
 
-  table.insert(global.todoList, input)
-  player.print("Added '" .. input .. "' to list!", addColor)
+  table.insert(global.todoList, todoItem)
+  player.print("TODO added: '" .. todoItem .. "'", ADD_COLOR)
+end
+
+-- removes a TODO item from the player's list
+function removeTodo(player, inputs)
+  player.clear_console()
+  if isListEmpty(global.todoList) then
+    player.print("Nothing TODO!", REMOVE_COLOR)
+  else
+    -- check that there are exactly two inputs
+    if #inputs == 2 then
+      local removeIndex = tonumber(inputs[2])
+      if removeIndex == nil then
+        player.print("The index provided must be a number!", NOTIFICATION_COLOR)
+      else
+        if #global.todoList < removeIndex then
+          player.print("There are not that many items in the TODO list!", NOTIFICATION_COLOR)
+        elseif removeIndex < 1 then
+          player.print("You cannot use an index that is less than one!", NOTIFICATION_COLOR)
+        else
+          local removedItem = global.todoList[removeIndex]
+          table.remove(global.todoList, removeIndex)
+          player.print("TODO removed: '" .. removedItem .. "'", REMOVE_COLOR)
+        end
+      end
+    elseif #inputs < 2 then
+      player.print("You must choose an item index to remove!", NOTIFICATION_COLOR)
+    else
+      player.print("Too many arguments provided - remove should only include two!", NOTIFICATION_COLOR)
+    end
+  end
 end
 
 
 -- runs each time any player writes a message to chat
 script.on_event(defines.events.on_console_chat,
   function(event)
-    local notificationColor = {r = 0.7, g = 0, b = 0.3, a = 1}
-    local removeColor = {r = 1, g = 0, b = 0, a = 1}
-    local addColor = {r = 0, g = 1, b = 0, a = 1}
-    local listColor = {r = 0, g = 0.7, b = 0.3, a = 1}
-
     local player = game.get_player(event.player_index)
     local input = event.message
     local inputs = mysplit(input, " ")
     local command = inputs[1]
     local playerName = player.name
 
-
+    -- based on the command entered take the appropriate action
     if command == "help" then
-
-      displayHelpMessage(player, notificationColor, listColor, addColor, removeColor)
-
-    -- shows all of the items on the player's TODO list
+      displayHelpMessage(player)
     elseif command == "list" then
-
-      showTodoList(player, global.todoList, listColor)
-
-    -- adds an item to the bottom of the player's TODO list
+      showTodoList(player, global.todoList)
     elseif command == "add" then
-
-      addTodo(player, input, addColor)
-
-    -- removes the item at the index provided from the player's TODO list
+      addTodo(player, inputs)
     elseif command == "remove" then
-
-      player.clear_console()
-      if isListEmpty(global.todoList) then
-        player.print("TODO List already empty!", removeColor)
-      else
-
-        -- check that there are exactly two inputs
-        if #inputs == 2 then
-
-          local removeIndex = tonumber(inputs[2])
-          if removeIndex == nil then
-            player.print("The index provided must be a number!", notificationColor)
-          else
-
-            if #global.todoList < removeIndex then
-              player.print("There are not that many items in the TODO list!", notificationColor)
-            else
-              table.remove(global.todoList, removeIndex)
-              player.print("Removed item: " .. removeIndex .. " from list!", removeColor)
-            end
-
-          end
-
-        elseif #inputs < 2 then
-
-          player.print("You must choose an item index to remove!", notificationColor)
-
-        else
-
-          player.print("Too many arguments provided - remove should only include two!", notificationColor)
-
-        end
-
-      end
-
+      removeTodo(player, inputs)
     end
 
-    --player.print("player: '" .. playerName .. "' command: '" .. command .. "' - count: ")
   end
 )
